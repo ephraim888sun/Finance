fc7c4e7f1dc1a772d44fc5bfca1a27fc31daaa89
Ephraim.Sun <ephraim888sun@gmail.com>
2021-07-19 09:41:10 -0500

    Update

--- Stock_Quote/Oil_Companies.py
+++ Stock_Quote/Oil_Companies.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
+print(plt.style.available)
+plt.style.use('Solarize_Light2')
+
 def cleandf(csv):
     csvsplit = csv.split('.')
     csvsplit = csvsplit[0]
@@ -23,7 +26,7 @@ df_RDS_B = cleandf('RDS_B.csv')
 df1 = pd.merge(left=df_XOM, right=df_COP, left_on='Date', right_on='Date')
 df2 = pd.merge(left=df_CVX, right=df_RDS_B, left_on='Date', right_on='Date')
 df = pd.merge(left=df1, right=df2, left_on='Date', right_on='Date')
-print(df.head(10))
+# print(df.head(10))
 # df2 = pd.merge(left = )
 
 df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
@@ -35,16 +38,25 @@ Highs_COP = df['High_COP'].resample('D').max()
 Highs_CVX = df['High_CVX'].resample('D').max()
 Highs_RDS_B = df['High_RDS_B'].resample('D').max()
 
-plt.plot(Highs_XOM, color = 'b', linestyle = (0, (5, 1)), label='Exxon Mobil (XOM)')
-plt.plot(Highs_COP, color = 'r', linestyle = 'dashed', label='ConocoPhillips (COP)')
-plt.plot(Highs_CVX, color = 'g', linestyle = 'solid', label='Chevron (CVX)')
-plt.plot(Highs_RDS_B, color = 'k', linestyle = 'dashdot', label='Royal Dutch Shell (RDS-B)')
+# plt.plot(Highs_XOM, color = 'b', linestyle = (0, (5, 1)), label='Exxon Mobil (XOM)')
+# plt.plot(Highs_COP, color = 'r', linestyle = 'dashed', label='ConocoPhillips (COP)')
+# plt.plot(Highs_CVX, color = 'g', linestyle = 'solid', label='Chevron (CVX)')
+# plt.plot(Highs_RDS_B, color = 'k', linestyle = 'dashdot', label='Royal Dutch Shell (RDS-B)')
+
+plt.plot(Highs_XOM, linewidth=1.0, label='Exxon Mobil (XOM)')
+plt.plot(Highs_COP, linewidth=1.0, label='ConocoPhillips (COP)')
+plt.plot(Highs_CVX, linewidth=1.0, label='Chevron (CVX)')
+plt.plot(Highs_RDS_B, linewidth=1.0, label='Royal Dutch Shell (RDS-B)')
 
 plt.xlabel('Year')
 plt.ylabel('Stock Price (USD)')
 plt.title('OIL & GAS COMPANIES')
 
 plt.legend()
+
+plt.tight_layout()
+
+# plt.savefig('Oil_Companies.png')
 
 plt.show()
 
